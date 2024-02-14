c47e54dbac2c8a6a19ace03f67b7c0dd9a3aa694
Igor Augusto <igoraugusto36s@gmail.com>
2024-02-14 02:22:49 -0300

    added translations for i18n

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,5 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import * as bootstrap from "bootstrap"
+import "jquery"
+import "notify-js-legacy"
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js"
+pin "jquery" # @3.7.1
+pin "notify-js-legacy" # @0.4.1
